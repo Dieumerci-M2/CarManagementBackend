42ce5fd7ab27a4eb1d7296df1ec94730b84997e1
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-06-20 13:49:43 +0200

    feat : run the app with Express framework

---
+++ app.js
@@ -0,0 +1,14 @@
+// Call express since node_module
+import express from 'express'
+// Call sequelize to get Connexion to DB
+//import { sequelize} from './config/db.js'
+// Call all environnement variables
+import {} from 'dotenv/config'
+// use express dependancies
+const app = express()
+// Add port for running app content
+const port = process.env.PORT || 6000
+
+// Add port listerning
+
+app.listen(port, ()=> console.log( `Notre app est lancée sur : http://localhost:${port}`))  
