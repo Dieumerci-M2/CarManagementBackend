85bb1d524cb44ddea5fdcfad47d8acd0f198a25c
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-06-24 17:06:48 +0200

    fix : make .env on .gotignore to keep some confidential element

--- config/db.js
+++ config/db.js
@@ -1,3 +1,22 @@
-const { Sequelize } = require( "sequelize" );
+import { Sequelize } from "sequelize" ;
 
-const sequelize = new Sequelize(process.env.POSTGRESQL_DB_URI)
+const sequelize = new Sequelize(
+    {
+        username: 'root',
+        password: '',
+        database: process.env.POSTGRESQL_DB_URI,
+        host: '127.0.0.1',
+        dialect: 'postgres'
+    }
+)
+
+const testDbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+export default { testDbConnection }
