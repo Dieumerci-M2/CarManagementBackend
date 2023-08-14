e5a09fae6a09e70a9e1ca0c20c432c92589d790c
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-08-14 15:28:41 +0200

    refactor : improve login code by adding error signal if the user add wrong information when he try to login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -4,26 +4,36 @@ import bcryp from "bcrypt"
 
 
 const login = ( req, res ) => {
-    try {
+    
         userModel.findOne( { where: { userName: req.body.userName } } )
             .then( used => {
+                if ( !used ) {
+                    res.status( 404 ).json( {
+                        message : `user not found`
+                    })
+                }
                 bcryp.compare( req.body.password, used.password )
                     .then( ispasswordValid => {
-                        if ( ispasswordValid ) {
-                            res.status( 200 ).json( {
+                        if ( !ispasswordValid ) {
+                            res.status( 401 ).json( {
+                               message : `incorrect password`
+                           }) 
+                        }
+                        res.status( 200 ).json( {
                                 message: `User Connected`,
                                 data: used
                             } )
-                        }
                     } )
             } )
- 
-    } catch {
-        res.status( 404 ).json( {
-            message: `This user is not registered`,
-            data : null
-        })
-    }
-}
+            .catch( error => {
+                res.status( 401 ).json( {
+                    message: `this user has not be connect. Try again`,
+                    data : error
+                })
+            })
+        } 
+    
+    
+
 
 export default login
