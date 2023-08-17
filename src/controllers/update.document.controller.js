1f68770fbe55d3cb6642eaf4ab6ce16bd92acd14
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-08-17 15:44:15 +0200

    feat : add update document controller for updating document if new things comes

--- src/controllers/update.document.controller.js
+++ src/controllers/update.document.controller.js
@@ -1,5 +1,29 @@
 import document from "../models/documentModel";
-
-const updateDoc = async ( req, res, next ) => {
-    
+import { ValidationError, UniqueConstraintError } from "sequelize";
+const updateDoc = async ( req, res) => {
+    const id = req.params.id
+    document.update( req.body, {
+        where : {id : id}
+    } )
+        .then( doc => {
+            if ( doc === null ) {
+                res.status( 404 ).json( {
+                message : `document does't exist. try to enter a new id`
+            })
+            }
+            res.status( 200 ).json( {
+                message: `document has modified successfuly`,
+                data : doc
+            })
+        } )
+        .catch( err => {
+            if(err instanceof ValidationError){
+                res.status(400).json({message: err.message, data: err})
+                }
+            if(err instanceof UniqueConstraintError){
+                res.status(400).json({message: err.message, data: err})
+                }
+            const message = `Le server ne repond pas veillez ressayez après quelques instants`     
+            res.status(500).json({message})
+        })
 }
